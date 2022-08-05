ecf07e9d3c43407132ca47abc5211fcaaf136ee6
tresabhi <coolabhigyaandeep07@gmail.com>
2022-08-05 09:47:31 +0000

    fix: dist reference from root

--- scripts/build.js
+++ scripts/build.js
@@ -1,14 +1,7 @@
 import { execSync } from 'child_process';
-import {
-  copyFileSync,
-  existsSync,
-  readFileSync,
-  rmSync,
-  writeFileSync
-} from 'fs';
+import { existsSync, readFileSync, rmSync, writeFileSync } from 'fs';
 
 const BUILD_DIR = 'dist';
-const INJECT_FROM_ROOT = ['package.json', 'README.md', 'LICENSE'];
 
 if (existsSync(BUILD_DIR)) {
   console.log(`removing already existing build directory "${BUILD_DIR}"`);
@@ -18,18 +11,12 @@ if (existsSync(BUILD_DIR)) {
 console.log('transpiling to JavaScript');
 execSync('npx tsc -p tsconfig.json');
 
-console.log(`injecting files from root into ${BUILD_DIR}`);
-INJECT_FROM_ROOT.forEach((fileName) => {
-  console.log(`injecting ${fileName}`);
-  copyFileSync(fileName, `${BUILD_DIR}/${fileName}`);
-});
-
 console.log('removing devDependencies');
-const packageRaw = readFileSync(`${BUILD_DIR}/package.json`);
+const packageRaw = readFileSync('package.json');
 const packageJSON = JSON.parse(packageRaw);
 
 delete packageJSON.devDependencies;
 
 const packageString = JSON.stringify(packageJSON);
 
-writeFileSync(`${BUILD_DIR}/package.json`, packageString);
+writeFileSync('package.json', packageString);
