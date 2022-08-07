46e017ea94933b334280b1d063a83fe34ef67910
tresabhi <coolabhigyaandeep07@gmail.com>
2022-08-07 17:04:16 +0000

    feat: lint

--- src/utilities/safeMinMaxInts.ts
+++ src/utilities/safeMinMaxInts.ts
@@ -1,4 +1,4 @@
 export const safeMinMaxInts = (min: number, max: number) => ({
-  safeMin: Math.round(Math.min(min ?? 8, max ?? 16)),
   safeMax: Math.round(Math.max(min ?? 8, max ?? 16)),
+  safeMin: Math.round(Math.min(min ?? 8, max ?? 16)),
 });
