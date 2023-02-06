241c8f7e53046224c3f7796cca56a15b8e282b7b
tresabhi <coolabhigyaandeep07@gmail.com>
2023-02-06 02:55:21 +0000

    feat: removed useless getNameFromKey

--- src/utilities/getNameFromKey/index.test.ts
+++
@@ -1,7 +0,0 @@
-import getNameFromKey from '.';
-
-test(`${getNameFromKey.name} gets name from keys`, () => {
-  expect(getNameFromKey('mauve6')).toBe('mauve');
-
-  expect(getNameFromKey('testTest12')).toBe('testTest');
-});
